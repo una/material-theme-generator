8753b0ed40efb45c6588b34aee7432668c0f8419
Una Kravets <unakravets@unakravets-imacpro.roam.corp.google.com>
2019-10-07 12:02:56 -0400

    surface color and color adjust working

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
   elem.previousElementSibling.appendChild(node)
 })
 
-// Color Visualization
+// TODO: Separate out these helper functions
 const rgbToHex = (col) => {
   if(col.charAt(0)=='r') {
     col=col.replace('rgb(','').replace(')','').split(',');
@@ -24,13 +24,65 @@ const rgbToHex = (col) => {
   }
 }
 
+const rgbToHsl = (r, g, b) => {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
+}
+
+const hslToHex = (h, s, l) => {
+  h /= 360;
+  s /= 100;
+  l /= 100;
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
 // Build out color variable visualizer
 const visualizeColorText = () => {
   [...document.querySelectorAll('.theme-summary-color')].forEach((elem) => {
-    let colorVal = elem.parentElement.firstElementChild.innerText
+    let colorVal = elem.parentElement.firstElementChild.innerText.replace(/:.*$/,"")
     const currentColor = getComputedStyle(document.documentElement).getPropertyValue(colorVal)
     elem.value = currentColor.trim()
-    console.log(currentColor, elem.value)
     elem.parentElement.querySelector('.varVal').innerText =  `: ${currentColor};`
 
     elem.addEventListener('change', (e) => {
@@ -77,26 +129,35 @@ const runColors = () => {
     img.addEventListener('load', function() {
       // same code as above
       let root = document.documentElement;
-      const pallete = colorThief.getPalette(img, 3);
+      const pallete = colorThief.getPalette(img, 4);
 
       // make 0 bg bc its usually duller/background ?
       const primary = pallete[1], 
             secondary = pallete[2],
-            bg = pallete[0];
+            bg = pallete[0],
+            surface = pallete[3];
 
-      const primaryColor = `rgb(${primary[0]}, ${primary[1]}, ${primary[2]})`
-      const secondaryColor = `rgb(${secondary[0]}, ${secondary[1]}, ${secondary[2]})`
-      const bgColor = `rgb(${bg[0]}, ${bg[1]}, ${bg[2]})`
+      const primaryColor = rgbToHex(`rgb(${primary[0]}, ${primary[1]}, ${primary[2]})`)
+      const secondaryColor = rgbToHex(`rgb(${secondary[0]}, ${secondary[1]}, ${secondary[2]})`)
+      const bgColor = rgbToHex(`rgb(${bg[0]}, ${bg[1]}, ${bg[2]})`)
+      console.log(surface)
+      const surfaceColorHSL = rgbToHsl(surface[0] * 360, surface[1] * 100, surface[2] * 100)
+      console.log(surfaceColorHSL)
+      const surfaceColor = hslToHex(surfaceColorHSL[0], surfaceColorHSL[1], surfaceColorHSL[2])
+      console.log(surfaceColor)
 
-      root.style.setProperty('--mdc-theme-primary', primaryColor);
-      root.style.setProperty('--mdc-theme-secondary', secondaryColor);
-      root.style.setProperty('--mdc-theme-background', bgColor);
+      root.style.setProperty('--mdc-theme-primary', primaryColor)
+      root.style.setProperty('--mdc-theme-secondary', secondaryColor)
+      root.style.setProperty('--mdc-theme-background', bgColor)
+      root.style.setProperty('--mdc-theme-surface', surfaceColor)
 
       // randomize border radius on small components
       root.style.setProperty('--mdc-shape-small', Math.floor(Math.random()*20)+'px');
 
       // Update colors in theme summary
-      visualizeColorText
+      visualizeColorText()
     });
   }
 }
+
+visualizeColorText()
